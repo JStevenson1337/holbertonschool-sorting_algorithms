b52a0ba3376373deefb7de46c3974b402a425b6e
jstevenson1337 <jeremy.m.stevenson@gmail.com>
2021-12-22 11:55:33 -0600

    changed variable data type

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,12 +8,12 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	int i, j;
+	unsigned int i, j;
 	int temp;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size -1; j++)
+		for (j = 0; j < size - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
