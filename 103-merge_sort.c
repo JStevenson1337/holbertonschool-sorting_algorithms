ae63280d0b0330ca54fbc7e641e9de7345ce67a6
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2022-05-08 23:06:49 -0500

    commit changes

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,61 +1,59 @@
 #include "sort.h"
-#include <stdlib.h>
-#include <stdio.h>
 
-/**
- * sort_helper - initiate merge sort
- * @array: array
- * @temp temp array
- * @size: size of the array
- */
-void sort_helper(int *array, int *temp, size_t size)
+void merge(int *array, int start, int mid, int end, int *temp)
 {
-	size_t half = size / 2, i = 0, j = 0, k;
-
-	if (size <= 2)
-		return;
-
-	sort_helper(array, temp, half);
-	sort_helper(array + half, temp + half, size - half);
-
-	printf("Merging...\n");
-	printf("[left]: ");
-	print_array(array, half);
-	printf("[right]: ");
-	print_array(array + half, size - half);
-	for (k = 0; k < size; k++)
-		if (j >= size - half || (i < half && array[i] < (array + half)[j]))
-		{
-			temp[k] = array[i];
-			i++;
-		}
-		else
-		{
-			temp[k] = (array + half)[j];
-			j++;
-		}
-	for (k = 0; k < size; k++)
-		array[k] = temp[k];
-	printf("[Done]: ");
-	print_array(array, size);
+    int i = start;
+    int j = mid + 1;
+    int k = 0;
+    while (i <= mid && j <= end)
+    {
+        if (array[i] <= array[j])
+        {
+            temp[k] = array[i];
+            i++;
+        }
+        else
+        {
+            temp[k] = array[j];
+            j++;
+        }
+        k++;
+    }
+    while (i <= mid)
+    {
+        temp[k] = array[i];
+        i++;
+        k++;
+    }
+    while (j <= end)
+    {
+        temp[k] = array[j];
+        j++;
+        k++;
+    }
+    for (i = start; i <= end; i++)
+    {
+        array[i] = temp[i - start];
+    }
 }
 
-/**
- * merge_sort - merge sort
- * @array: array to be sorted
- * @size: size of the array
- */
 void merge_sort(int *array, size_t size)
 {
-	int *temp;
-
-	if (!array || size < 2)
-		return;
-
-	temp = malloc(sizeof(*temp) * size);
-	if (!temp)
-		return;
-
-	sort_helper(array, temp, size);
-	free(temp);
+    int *temp = malloc(sizeof(int) * size);
+    int start, end;
+    if (size < 2)
+    {
+        return;
+    }
+    start = 0;
+    end = size - 1;
+    while (start < end)
+    {
+        merge(array, start, (start + end) / 2, end, temp);
+        start = (start + end) / 2 + 1;
+        end = end - 1;
+    }
+    free(temp);
 }
+
+
