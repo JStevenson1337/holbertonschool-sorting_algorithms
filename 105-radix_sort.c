2e9d526a0ab9ff1f26a1c121e63baed68d828e42
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2022-06-11 11:08:23 -0500

    fix: betty

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -1,12 +1,12 @@
 #include "sort.h"
 
 /**
- * max_int - find the max int in an array
- * @arr: array of ints
- * @n: size of arrays
- *
- * Return: max int in array
- */
+ * max_int - Finds the largest number in an array of integers
+ * @arr: The array to search through
+ * @n: The number of digits of the array (to look through on the left side)
+ * Return: The largest number from the array
+*/
+
 int max_int(int arr[], int n)
 {
 	int i;
@@ -29,47 +29,41 @@ int max_int(int arr[], int n)
 
 void radix_sort(int arr[], int n)
 {
-	int digits = 0, i, j, maximum, num, power;
+	int digits = 0, i, j, max, num, pow;
 	int count[10];
 	int *new_array;
 
 	if (arr == NULL || n < 2)
 		return;
-	maximum = max_int(arr, n);
-	while (maximum > 0)
+	max = max_int(arr, n);
+	while (max > 0)
 	{
 		digits++;
-		maximum /= 10;
+		max /= 10;
 	}
-	new_array = malloc(sizeof(int) * n);
-	if (new_array == NULL)
-		return;
-	for (i = 0; i < 10; i++)
-		count[i] = 0;
-	for (i = 0; i < n; i++)
+	pow = 1;
+	for (i = 0; i < digits; i++)
 	{
-		num = arr[i];
-		power = 1;
-		for (j = 0; j < digits; j++)
+		new_array = malloc(sizeof(int) * n);
+		for (j = 0; j < 10; j++)
+			count[j] = 0;
+		for (j = 0; j < n; j++)
 		{
-			count[num / power % 10]++;
-			power *= 10;
+			num = (arr[j] / pow) % 10;
+			count[num]++;
 		}
-	}
-	for (i = 1; i < 10; i++)
-		count[i] += count[i - 1];
-	for (i = n - 1; i >= 0; i--)
-	{
-		num = arr[i];
-		power = 1;
-		for (j = 0; j < digits; j++)
+		for (j = 1; j < 10; j++)
+			count[j] += count[j - 1];
+		for (j = n - 1; j >= 0; j--)
 		{
-			new_array[count[num / power % 10] - 1] = num;
-			count[num / power % 10]--;
-			power *= 10;
+			num = (arr[j] / pow) % 10;
+			new_array[count[num] - 1] = arr[j];
+			count[num]--;
 		}
+		for (j = 0; j < n; j++)
+			arr[j] = new_array[j];
+		pow *= 10;
+		free(new_array);
+		print_array(arr, n);
 	}
-	for (i = 0; i < n; i++)
-		arr[i] = new_array[i];
-	free(new_array);
 }
